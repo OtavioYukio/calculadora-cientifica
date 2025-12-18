f359a9e49a0a30a416f08d1137afd482d4d10f5f
OtavioYukio <yukiootavio@gmail.com>
2025-12-18 14:11:47 -0300

    feat: criação da classe HelpUI e verificações em caso de inserção de comandos

---
+++ src/ui/HelpUI.java
@@ -0,0 +1,19 @@
+package ui;
+
+public class HelpUI {
+    public static void start() {
+        System.out.println("--- HELP ---" +
+                "\nx + y = addition" +
+                "\nx - y = subtraction" +
+                "\nx * y = multiplication" +
+                "\nx / y = division" +
+                "\nx ^ y = exponentiation" +
+                "\nsqrt = square root" +
+                "\ncos(x) = cosine" +
+                "\nsin(x) = sine" +
+                "\ntan(x) = tangent" +
+                "\nlog(x) = logarithm" +
+                "\nx % y = percentage" +
+                "\nx! = factorial");
+    }
+}
--- src/ui/MenuUI.java
+++ src/ui/MenuUI.java
@@ -5,9 +5,20 @@ import java.util.Locale;
 
 public class MenuUI {
     public static void start() {
-        System.out.println("Enter a mathematical expression (Enter /help " +
-                "to learn about commands and how to perform possible" +
-                "operations.)");
-
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter a mathematical expression (Enter /help " +
+                    "to learn about commands and how to perform possible" +
+                    "operations.) (Enter /exit to exit)");
+            String expression = scanner.nextLine();
+            if (expression == "/help") {
+                HelpUI.start();
+                scanner.nextLine();
+            }
+            if (expression == "/exit") {
+                scanner.close();
+                System.exit(0);
+            }
+        }
     }
 }
