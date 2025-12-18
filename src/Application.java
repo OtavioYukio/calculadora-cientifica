9ef8e3c58698b08e41e649e5b7679d7c0fdf1582
OtavioYukio <yukiootavio@gmail.com>
2025-12-18 11:18:18 -0300

    feat: criada classe Application que chama o metodo start da classe MenuUI

---
+++ src/Application.java
@@ -0,0 +1,7 @@
+import ui.MenuUI;
+
+public class Application {
+    public static void main(String[] args) {
+        MenuUI.start();
+    }
+}
---
+++ src/ui/MenuUI.java
@@ -0,0 +1,13 @@
+package ui;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class MenuUI {
+    public static void start() {
+        System.out.println("Enter a mathematical expression (Enter /help " +
+                "to learn about commands and how to perform possible" +
+                "operations.)");
+
+    }
+}
